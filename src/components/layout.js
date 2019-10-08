fa3b488a8a1d27682cb0adb4bd73caae3fc684f0
Muhammad Rusdi <muhammadrusdi@icloud.com>
2019-10-08 14:57:20 +0000

    change page

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,52 +1,118 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
-import PropTypes from "prop-types"
+import styled from "styled-components"
 import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+import Container from 'Components/container'
+import { Helmet } from "react-helmet"
+import "./styles.css"
+import Particles from "react-particles-js"
+import img1 from "Images/img-1.png"
+import img2 from "Images/img-2.png"
+import img3 from "Images/img-3.png"
 
-import Header from "./header"
-import "./layout.css"
+const Nav = styled.nav`
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0
+  height: 60px;
+`
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
+const Logo = styled.div`
+  display: flex;
+  padding-top: 20px;
+  align-items: center;
+`
+
+const Layout = ({children}) => {
+
+  const data = useStaticQuery(
+    graphql`
+      query {
+        logo: file(relativePath: {eq: "dauroh-logo-4.png"}) {
+          childImageSharp {
+            fixed(width: 40) {
+              ...GatsbyImageSharpFixed
+            }
+          }
         }
       }
-    }
-  `)
-
+    `
+  )
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+      <Helmet>
+        <title>Dauroh</title>
+      </Helmet>
+      <Particles
+        style={{position: "absolute", zIndex: -1}}
+        params={{
+          "particles": {
+              "number": {
+                  "value": 10,
+                  "density": {
+                      "enable": true,
+                      "value_area": 800
+                  }
+              },
+              "line_linked": {
+                  "enable": false
+              },
+              "move": {
+                  "speed": 0.7,
+                  "out_mode": "out"
+              },
+              "shape": {
+                  "type": [
+                      "images",
+                  ],
+                  "images": [
+                      {
+                          "src": img1,
+                          "height": 20,
+                          "width": 23
+                      },
+                      {
+                          "src": img2,
+                          "height": 20,
+                          "width": 20
+                      },
+                      {
+                          "src": img3,
+                          "height": 20,
+                          "width": 20
+                      },
+                  ]
+              },
+              "color": {
+                  "value": "#CCC"
+              },
+              "size": {
+                  "value": 30,
+                  "random": false,
+                  "anim": {
+                      "enable": true,
+                      "speed": 4,
+                      "size_min": 10,
+                      "sync": false
+                  }
+              }
+          },
+          "retina_detect": false
+      }} />
+      <Nav>
+      <Container>
+        <Logo>
+          {/* <span><Img fixed={data.logo.childImageSharp.fixed}/></span> */}
+          <h1>Paradox</h1>
+        </Logo>
+        </Container>
+      </Nav>
+      <div>
+        {children}
       </div>
     </>
   )
-}
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
 }
 
 export default Layout
